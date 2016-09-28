02449754dd923a7b7b32c56c3fe128eaa190d805
Leem ChaeHoon <im.fehead@gmail.com>
2016-09-29 00:51:35 +0900

    vimrc 추가 make tags 병렬 실행하게 변경.

--- make_tags.sh
+++ make_tags.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 if [ "$1" != "emacs" ];then
 	echo "make tags ARCH=arm"
-	make tags ARCH=arm
+	make tags ARCH=arm &
 else
 	echo "make TAGS ARCH=arm"
-	make TAGS ARCH=arm
+	make TAGS ARCH=arm &
 fi
 
+TAGS_PID=$!
 echo "make cscope ARCH=arm"
 make cscope ARCH=arm
+
+wait ${TAGS_PID}

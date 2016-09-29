7476a54dbff1f3b366fe313e50481c3223e3ed08
Leem ChaeHoon <im.fehead@gmail.com>
2016-09-29 21:58:33 +0900

    ctags, cscope 병렬 처리후 완료할때까지 기다리게 수정.

--- make_tags.sh
+++ make_tags.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-if [ "$1" != "emacs" ];then
-	echo "make tags ARCH=arm"
-	make tags ARCH=arm &
-else
-	echo "make TAGS ARCH=arm"
-	make TAGS ARCH=arm &
+TAGS="tags"
+if [ "$1" == "emacs" ];then
+	TAGS="TAGS"
 fi
 
+echo "make ${TAGS} ARCH=arm"
+make ${TAGS} ARCH=arm &
 TAGS_PID=$!
-echo "make cscope ARCH=arm"
-make cscope ARCH=arm
 
-wait ${TAGS_PID}
+echo "make cscope ARCH=arm"
+make cscope ARCH=arm &
+CS_PID=$!
+
+wait ${TAGS_PID} ${CS_PID}
+
+echo done.
